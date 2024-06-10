86315b37ba59a83dc343dd6369becb01f70111ec
zincsabian <2773467721@qq.com>
2024-06-11 01:39:02 +0800

    feature: shared_ptr

--- priority_queue/priority_queue.hpp
+++ priority_queue/priority_queue.hpp
@@ -1,63 +1,44 @@
-#include <vector>
 #include <deque>
-#include <iostream>
-#include <functional>
 #include <algorithm>
 
 template <class T = int> 
-class Node {
+class Node: public std::enable_shared_from_this<Node<T>> {
 public:
     T value;
-    Node<T>* parent;
-    Node<T>* left_child;
-    Node<T>* right_child;
+    std::shared_ptr<Node<T>> parent;
+    std::shared_ptr<Node<T>> left_child;
+    std::shared_ptr<Node<T>> right_child;
     bool inq;
 
-    Node(T value_) : value(value_){
-        parent = nullptr;
-        left_child = nullptr;
-        right_child = nullptr;
-        inq = false;
-    };
+    Node(T value_) : value(value_), parent(nullptr), left_child(nullptr), right_child(nullptr), inq(false) {}
+
     Node() = default;
 
     // 上调
     void heapify_up() {
-        auto* cur = this;
-        for(; cur->parent != nullptr; ) {
-            if (cur->parent->value < cur->value)
-                std::swap(cur->value, cur->parent->value);
-            else 
-                break; 
+        auto cur = this->shared_from_this();
+        while (cur->parent != nullptr && cur->parent->value < cur->value) {
+            std::swap(cur->value, cur->parent->value);
             cur = cur->parent;
         }
     }
 
     // 沉底
     void heapify_down() {
-        auto* cur = this;
-        for(; cur->left_child != nullptr; ) {
-            auto* bigger = cur->left_child; 
-            if (cur->right_child != nullptr && cur->right_child->value > bigger->value) { 
-                bigger = cur->right_child;
-            }
-            if (bigger->value > cur->value) { 
-                std::swap(bigger->value, cur->value); 
-                cur = bigger; 
-            }
-            else {
+        auto cur = this->shared_from_this();
+        while (cur->left_child != nullptr) {
+            auto& bigger = (cur->right_child != nullptr && cur->right_child->value > cur->left_child->value) ? cur->right_child : cur->left_child;
+            if (bigger->value > cur->value) {
+                std::swap(cur->value, bigger->value);
+                cur = bigger;
+            } else {
                 break;
             }
         }
     }
 
-    ~Node() {
-        auto* cur = this;
-        cur->parent = nullptr;
-        cur->left_child = nullptr;
-        cur->right_child = nullptr;
-        inq = false;
-    }
+    // 简化析构函数，智能指针会自动处理
+    ~Node() = default;
 };
 
 // 大根堆
@@ -74,14 +55,14 @@ public:
 
     void push(const T& x) {
         n++;
-        Node<T>* newnode = new Node<T>(x);
+        std::shared_ptr<Node<T>> newnode = std::make_shared<Node<T>>(x);
         if (root == nullptr) {
             root = newnode;
             leave.push_back(newnode);
             newnode->inq = true;
         } 
         else {
-            auto* leaf = leave.front();
+            auto leaf = leave.front();
             if (leaf->left_child == nullptr) {
                 leaf->left_child = newnode;
                 newnode->parent = leaf;
@@ -113,16 +94,15 @@ public:
         }
 
         std::swap(root->value, leave.back()->value);
-        auto* tmp = leave.back();
+        auto tmp = leave.back();
         leave.pop_back();
         n--;
 
         if (root == tmp) {
-            delete root;
             root = nullptr;
         } 
         else {
-            auto* parent = tmp->parent;
+            auto parent = tmp->parent;
             if (parent->left_child == tmp) {
                 parent->left_child = nullptr;
             } else {
@@ -134,7 +114,6 @@ public:
                 leave.push_front(parent);
             }
 
-            delete tmp;
             root->heapify_down();
         }
     }
@@ -149,6 +128,6 @@ public:
 
 private:
     size_t n;
-    Node<T>* root;
-    std::deque<Node<T>*> leave;  // 维护所有叶子结点
+    std::shared_ptr<Node<T>> root;
+    std::deque<std::shared_ptr<Node<T>>> leave;  // 维护所有叶子结点
 };
